ffd643649907afe5310457792eb001dc1bf30c9f
KevinKossi <mpungakevin@hotmail.com>
2023-06-23 13:26:32 +0200

    donwload

--- graph.py
+++ graph.py
@@ -169,3 +169,9 @@ if __name__ == '__main__':
 
 
 #[ERROR] [1687517702.038038, 97.835000]: Action server not available!
+
+# $ cd ~/catkin_ws/src/
+# $ git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+# $ git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+# $ git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+# $ cd ~/catkin_ws && catkin_make
